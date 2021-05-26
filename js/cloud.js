0241b7b30e7691de43ae8168f5d238b1f9816b70
Felicia Zhang <t-fezhang@microsoft.com>
2021-05-26 17:05:37 -0400

    add CloudScript

---
+++ js/cloud.js
@@ -0,0 +1,10 @@
+handlers.addUserVirtualCurrency = function (args, context) {
+    var amount = args.amount;
+    var virtualCurrency = args.virtualCurrency;
+    var request = {
+        PlayFabId: currentPlayerId, 
+        VirtualCurrency: virtualCurrency,
+        Amount: amount
+    };
+    server.AddUserVirtualCurrency(request);
+};
