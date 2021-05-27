4910a4fdde41ab257b6ab246b613d81773f43acf
Felicia Zhang <t-fezhang@microsoft.com>
2021-05-27 16:16:26 -0400

    fix cloud

--- cloud.js
+++ cloud.js
@@ -25,7 +25,7 @@ handlers.syncInventoryToCatalog = function (args, context) {
                 var request = {
                     ItemInstanceId: instanceId,
                     PlayFabId: currentPlayerId,
-                    Data: {"Description": item.Description, "CustomData": item.CustomData}
+                    Data: {"Description": item.Description, "image": JSON.parse(item.CustomData)['image']}
                 }
                 server.UpdateUserInventoryItemCustomData(request);
             })
