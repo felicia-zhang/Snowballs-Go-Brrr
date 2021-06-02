9cadf97cbde4f471fe6a895266bcbdca44ed59ed
Jordan Roher <jordan.roher@microsoft.com>
2021-06-02 09:59:35 -0700

    Moved Google login to app Added code formatting plugin Still slightly broken

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,75 @@
 import { PhaserGame } from "./components/phaser";
-import React from 'react';
+import React from "react";
+import { PlayFab, PlayFabClient } from "playfab-sdk";
+import GoogleLogin, { GoogleLoginResponse } from "react-google-login";
 
-class App extends React.PureComponent {
-  constructor(props) {
-    super(props)
-  }
+interface IState {
+	game: PhaserGame;
+	isGoogleSignedIn: boolean;
+}
 
-  componentDidMount() {
-    var game = new PhaserGame();
-  }
+class App extends React.PureComponent<any, IState> {
+	constructor(props: any) {
+		super(props);
 
-  render() {
-    return (
-      <></>
-    );
-  }
+		this.state = {
+			game: null,
+			isGoogleSignedIn: false,
+		};
+
+		PlayFab.settings.titleId = "7343B";
+	}
+
+	componentDidMount() {
+		this.setState({
+			game: new PhaserGame(),
+		});
+	}
+
+	componentWillUnmount() {
+		this.state.game.destroy(true);
+	}
+
+	private onGoogleSuccess = (a: GoogleLoginResponse) => {
+		this.setState({
+			isGoogleSignedIn: true,
+		});
+		console.log("Google success?", a);
+		PlayFabClient.LoginWithGoogleAccount(
+			{
+				ServerAuthCode: a.accessToken,
+				CreateAccount: true,
+			},
+			(a, b) => {
+				console.log("Callback", a, b);
+				this.onPlayFabResponse(a, b);
+			}
+		);
+	};
+
+	private onPlayFabResponse(response, error) {
+		// testing to see if below api call works
+		PlayFabClient.GetCatalogItems({ CatalogVersion: "1" }, (r, e) => console.log(r, e));
+	}
+
+	private onGoogleFailure(a: any) {
+		console.log("Something went wrong", a);
+	}
+
+	render() {
+		return (
+			<div>
+				<GoogleLogin
+					clientId="168518881059-39uvi2d24ev5rjscb6go5q4cljni1tgd.apps.googleusercontent.com"
+					buttonText="Login"
+					onSuccess={this.onGoogleSuccess}
+					onFailure={this.onGoogleFailure}
+					cookiePolicy={"single_host_origin"}
+					isSignedIn={this.state.isGoogleSignedIn}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/login.ts
+++ src/components/login.ts
@@ -1,47 +1,28 @@
-import { PlayFabClient } from 'playfab-sdk';
-import { fontFamily } from '../utils/font'
-import InputText from 'phaser3-rex-plugins/plugins/inputtext.js';
-import * as PlayFab from "playfab-sdk/Scripts/PlayFab/PlayFabClient.js";
+import { fontFamily } from "../utils/font";
+import InputText from "phaser3-rex-plugins/plugins/inputtext.js";
 
 class LoginScene extends Phaser.Scene {
-    playerName: string
-    constructor() {
-        super('Login');
-    }
+	playerName: string;
+	constructor() {
+		super("Login");
+	}
 
-    create() {
-        this.add.image(400, 300, 'sky');
-        var inputText = new InputText(this, 400, 300, 100, 20, {
-            type: 'textarea',
-            text: 'hello world',
-            fontFamily: fontFamily
-        }).on('textchange', function (inputText) {
-            this.playerName = inputText.text
-        });
-        this.add.existing(inputText);
+	create() {
+		this.add.image(400, 300, "sky");
+		var inputText = new InputText(this, 400, 300, 100, 20, {
+			type: "textarea",
+			text: "hello world",
+			fontFamily: fontFamily,
+		}).on("textchange", function (inputText) {
+			this.playerName = inputText.text;
+		});
+		this.add.existing(inputText);
 
-        // const _gapi = window.gapi;
-        // _gapi.load('auth2', () => {
-        //   (async () => { 
-        //     const _googleAuth = _gapi.auth2.init({
-        //         client_id: "168518881059-39uvi2d24ev5rjscb6go5q4cljni1tgd.apps.googleusercontent.com"
-        //     });
-        //     _googleAuth.attachClickHandler(document.querySelector('.divId'), {},
-        //         function(googleUser) {
-        //             console.log(googleUser.getBasicProfile().getName())
-        //         }, function(error) {
-        //           alert(JSON.stringify(error, undefined, 2));
-        //         });
-        //   })();
-        // });
-
-
-        const login = this;
-        const loginButton = this.add.text(700, 400, "login", { fontFamily: fontFamily });
-        loginButton.setInteractive({ useHandCursor: true }).on("pointerdown", () => {
-            login.scene.start('Game');
-        })
-    }
+		const loginButton = this.add.text(700, 400, "login", { fontFamily: fontFamily });
+		loginButton.setInteractive({ useHandCursor: true }).on("pointerdown", () => {
+			this.scene.start("Game");
+		});
+	}
 }
 
 export default LoginScene;
