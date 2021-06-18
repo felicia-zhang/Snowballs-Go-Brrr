9dfb8730d0fa0cad7f914a88d861bb38d543ed48
Felicia Zhang <t-fezhang@microsoft.com>
2021-06-18 11:41:01 -0400

    format leaderboard

--- src/components/leaderboard.ts
+++ src/components/leaderboard.ts
@@ -1,8 +1,8 @@
 import { PlayFabClient } from "playfab-sdk";
 import { fontFamily } from "../utils/font";
+import RoundRectangle from "phaser3-rex-plugins/plugins/roundrectangle.js";
 
 class LeaderboardScene extends Phaser.Scene {
-	leaderboardName: Phaser.GameObjects.Text;
 	list: Phaser.GameObjects.Container;
 	constructor() {
 		super("Leaderboard");
@@ -10,29 +10,30 @@ class LeaderboardScene extends Phaser.Scene {
 
 	create() {
 		this.add.image(400, 300, "sky");
-		this.add.text(300, 9, "Leaderboard", { fontFamily: fontFamily });
-		this.leaderboardName = this.add.text(300, 50, "", { fontFamily: fontFamily });
-		this.add.text(200, 80, "PLACE", { fontFamily: fontFamily });
-		this.add.text(300, 80, "NAME", { fontFamily: fontFamily });
-		this.add.text(500, 80, "STATISTIC", { fontFamily: fontFamily });
-		this.list = this.add.container(200, 110, []);
-		this.showLeaderboard("5");
+		const bg = this.add.existing(new RoundRectangle(this, 400, 300, 380, 450, 15, 0x16252e));
+		this.list = this.add.container(230, 130, []);
 
 		this.add
-			.text(80, 400, "current_snowballs", { fontFamily: fontFamily })
+			.text(80, 50, "current_snowballs", { fontFamily: fontFamily })
 			.setInteractive({ useHandCursor: true })
 			.on("pointerup", () => this.showLeaderboard("5"));
 		this.add
-			.text(80 + 200, 400, "total_added_snowballs", { fontFamily: fontFamily })
+			.text(80 + 200, 50, "total_added_snowballs", { fontFamily: fontFamily })
 			.setInteractive({ useHandCursor: true })
 			.on("pointerup", () => this.showLeaderboard("6"));
 		this.add
-			.text(80 + 400, 400, "total_manual_penguin_clicks", { fontFamily: fontFamily })
+			.text(80 + 400, 50, "total_manual_penguin_clicks", { fontFamily: fontFamily })
 			.setInteractive({ useHandCursor: true })
 			.on("pointerup", () => this.showLeaderboard("7"));
+		this.add.text(400, 16, "Leaderboard", { fontFamily: fontFamily }).setOrigin(0.5, 0.5).setAlign("center");
+		this.add.text(230, 80, "RANK", { fontFamily: fontFamily });
+		this.add.text(300, 80, "PLAYER", { fontFamily: fontFamily });
+		this.add.text(500, 80, "STATISTIC", { fontFamily: fontFamily });
+		this.showLeaderboard("5");
 
 		this.add
-			.text(700, 550, "MENU", { fontFamily: fontFamily })
+			.text(784, 584, "MENU", { fontFamily: fontFamily })
+			.setOrigin(1, 1)
 			.setInteractive({ useHandCursor: true })
 			.on("pointerup", () => {
 				this.scene.start("Menu");
@@ -40,17 +41,16 @@ class LeaderboardScene extends Phaser.Scene {
 	}
 
 	showLeaderboard(stat: string) {
-		this.leaderboardName.setText(stat);
 		this.list.removeAll(true);
 		PlayFabClient.GetLeaderboard({ StatisticName: stat, StartPosition: 0 }, (error, result) => {
 			const players = result.data.Leaderboard;
-			const texts: Phaser.GameObjects.Text[] = [];
 			players.forEach((player, i) => {
-				texts.push(this.add.text(0, i * 20, (i + 1).toString(), { fontFamily: fontFamily }));
-				texts.push(this.add.text(100, i * 20, player.DisplayName, { fontFamily: fontFamily }));
-				texts.push(this.add.text(300, i * 20, player.StatValue.toString(), { fontFamily: fontFamily }));
+				const bg = this.add.existing(new RoundRectangle(this, 170, i * 70 + 6, 340, 40, 15, 0x2e5767));
+				const rankText = this.add.text(10, i * 70, (i + 1).toString(), { fontFamily: fontFamily });
+				const playerText = this.add.text(50, i * 70, player.DisplayName, { fontFamily: fontFamily });
+				const statText = this.add.text(250, i * 70, player.StatValue.toString(), { fontFamily: fontFamily });
+				this.list.add([bg, rankText, playerText, statText]);
 			});
-			this.list.add(texts);
 		});
 	}
 
