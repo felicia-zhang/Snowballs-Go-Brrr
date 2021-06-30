1fb883305d3ae3b48acd9e50e3d261f4c873fa4d
Felicia Zhang <t-fezhang@microsoft.com>
2021-06-30 11:20:46 -0700

    add text hover animation to menu scene

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -20,22 +20,43 @@ class MenuScene extends AScene {
 				this.registry.set("IC", result.data.VirtualCurrency.IC);
 				this.registry.set("Inventories", result.data.Inventory);
 
-				this.add
+				const startButtonUnderline = this.add.line(400, 200, 400, 200, 400, 200, 0xffffff).setAlpha(0);
+				const startButton = this.add
 					.text(400, 200, "START", textStyle)
 					.setOrigin(0.5, 0.5)
 					.setAlign("center")
 					.setInteractive({ useHandCursor: true })
+					.on("pointerover", () => {
+						startButtonUnderline
+							.setTo(0, 0, startButton.width, 0)
+							.setPosition(400 - startButton.width / 2, 200 + startButton.height / 2)
+							.setAlpha(1);
+					})
+					.on("pointerout", () => {
+						startButtonUnderline.setAlpha(0);
+					})
 					.on("pointerup", () => {
 						this.cameras.main.fadeOut(500, 0, 0, 0);
 						this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
 							this.scene.start("Map");
 						});
 					});
-				this.add
+
+				const leaderboardButtonUnderline = this.add.line(400, 250, 400, 250, 400, 250, 0xffffff).setAlpha(0);
+				const leaderboardButton = this.add
 					.text(400, 250, "LEADERBOARD", textStyle)
 					.setOrigin(0.5, 0.5)
 					.setAlign("center")
 					.setInteractive({ useHandCursor: true })
+					.on("pointerover", () => {
+						leaderboardButtonUnderline
+							.setTo(0, 0, leaderboardButton.width, 0)
+							.setPosition(400 - leaderboardButton.width / 2, 250 + leaderboardButton.height / 2)
+							.setAlpha(1);
+					})
+					.on("pointerout", () => {
+						leaderboardButtonUnderline.setAlpha(0);
+					})
 					.on("pointerup", () => {
 						this.cameras.main.fadeOut(500, 0, 0, 0);
 						this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
