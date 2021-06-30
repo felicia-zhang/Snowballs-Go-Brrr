2d316618303a5ab34918d147b3a654c7b36632c8
Felicia Zhang <t-fezhang@microsoft.com>
2021-06-30 15:02:28 -0700

    add button and popup to reset

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -1,8 +1,18 @@
 import { PlayFabClient } from "playfab-sdk";
-import { textStyle } from "../utils/constants";
+import {
+	buttonClick,
+	buttonHover,
+	buttonNormal,
+	darkBackgroundColor,
+	overlayDepth,
+	popupDepth,
+	textStyle,
+} from "../utils/constants";
+import RoundRectangle from "phaser3-rex-plugins/plugins/roundrectangle.js";
 import AScene from "./AScene";
 
 class MenuScene extends AScene {
+	resetConfirmationContainer: Phaser.GameObjects.Container;
 	constructor() {
 		super("Menu");
 	}
@@ -11,6 +21,7 @@ class MenuScene extends AScene {
 		this.cameras.main.fadeIn(500, 0, 0, 0);
 		this.add.image(400, 300, "sky");
 		this.add.text(400, 16, "MENU", textStyle).setAlign("center").setOrigin(0.5, 0);
+		this.makeResetConfirmationContainer();
 
 		PlayFabClient.GetCatalogItems({ CatalogVersion: "1" }, (error, result) => {
 			this.registry.set("CatalogItems", result.data.Catalog);
@@ -41,6 +52,7 @@ class MenuScene extends AScene {
 							this.scene.start("Map");
 						});
 					});
+				this.interactiveObjects.push(startButton);
 
 				const leaderboardButtonUnderline = this.add.line(400, 250, 400, 250, 400, 250, 0xffffff).setAlpha(0);
 				const leaderboardButton = this.add
@@ -63,8 +75,105 @@ class MenuScene extends AScene {
 							this.scene.start("Leaderboard");
 						});
 					});
+				this.interactiveObjects.push(leaderboardButton);
+
+				const resetButtonUnderline = this.add.line(400, 300, 400, 300, 400, 300, 0xffffff).setAlpha(0);
+				const resetButton = this.add
+					.text(400, 300, "RESET", textStyle)
+					.setOrigin(0.5, 0.5)
+					.setAlign("center")
+					.setInteractive({ useHandCursor: true })
+					.on("pointerover", () => {
+						resetButtonUnderline
+							.setTo(0, 0, resetButton.width, 0)
+							.setPosition(400 - resetButton.width / 2, 300 + resetButton.height / 2)
+							.setAlpha(1);
+					})
+					.on("pointerout", () => {
+						resetButtonUnderline.setAlpha(0);
+					})
+					.on("pointerup", () => {
+						this.interactiveObjects.forEach(object => object.disableInteractive());
+						this.add.tween({
+							targets: [this.resetConfirmationContainer],
+							ease: "Sine.easeIn",
+							duration: 500,
+							alpha: 1,
+							callbackScope: this,
+						});
+					});
+				this.interactiveObjects.push(resetButton);
 			});
 		});
+	}
+
+	makeResetConfirmationContainer() {
+		const overlay = this.add.rectangle(0, 0, 800, 600, 0x000000).setDepth(overlayDepth).setAlpha(0.6);
+		const bg = this.add.existing(new RoundRectangle(this, 0, 0, 520, 340, 15, darkBackgroundColor));
+		this.resetConfirmationContainer = this.add.container(400, 300, [overlay, bg]).setDepth(popupDepth).setAlpha(0);
+		this.makeButton("yes");
+		this.makeButton("cancel");
+	}
+
+	makeButton(type: string) {
+		const highlight = this.add.existing(new RoundRectangle(this, 0, 0, 58, 36, 10, 0xffffff)).setAlpha(0);
+		const buttonText = this.add.text(0, 0, "", textStyle).setAlign("center").setOrigin(0.5, 0.5);
+		const button = this.add
+			.existing(new RoundRectangle(this, 0, 0, 58, 36, 10, buttonNormal))
+			.setInteractive({ useHandCursor: true })
+			.on("pointerover", () => {
+				button.setFillStyle(buttonHover, 1);
+			})
+			.on("pointerout", () => {
+				button.setFillStyle(buttonNormal, 1);
+			})
+			.on("pointerdown", () => {
+				button.setFillStyle(buttonClick, 1);
+				this.add.tween({
+					targets: [highlight],
+					ease: "Sine.easeIn",
+					props: {
+						width: {
+							value: 63,
+							duration: 150,
+							ease: "Sine.easeIn",
+						},
+						height: {
+							value: 41,
+							duration: 150,
+							ease: "Sine.easeIn",
+						},
+						alpha: {
+							value: 0.3,
+							duration: 150,
+							ease: "Sine.easeIn",
+						},
+					},
+					callbackScope: this,
+				});
+			});
+		if (type === "yes") {
+			highlight.x = 0;
+			buttonText.setText("YES").setX(0);
+			button.setX(0);
+		} else {
+			highlight.x = 120;
+			buttonText.setText("CANCEL").setX(120);
+			button.setX(120).on("pointerup", () => {
+				this.add.tween({
+					targets: [this.resetConfirmationContainer],
+					ease: "Sine.easeIn",
+					duration: 100,
+					alpha: 0,
+					onComplete: () => {
+						this.interactiveObjects.forEach(object => object.setInteractive({ useHandCursor: true }));
+						highlight.setAlpha(0);
+					},
+					callbackScope: this,
+				});
+			});
+		}
+		this.resetConfirmationContainer.add([highlight, button, buttonText]);
 	}
 
 	update() {
