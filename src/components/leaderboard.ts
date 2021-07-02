d4f82baab5f616bbe1065a16c1858108c0e7d7bf
Felicia Zhang <t-fezhang@microsoft.com>
2021-07-02 08:27:37 -0400

    update leaderboard layout

--- src/components/leaderboard.ts
+++ src/components/leaderboard.ts
@@ -15,28 +15,31 @@ class LeaderboardScene extends AScene {
 		this.list = this.add.container(400, 300, []);
 		this.add.text(400, 16, "LEADERBOARD", textStyle).setAlign("center").setOrigin(0.5, 0);
 
-		PlayFabClient.GetLeaderboard({ StatisticName: "snowballs", StartPosition: 0 }, (error, result) => {
-			const darkBg = this.add.existing(new RoundRectangle(this, 0, 0, 480, 400, 15, darkBackgroundColor));
-			const players = result.data.Leaderboard;
-			players.forEach((player, i) => {
-				const lightBg = this.add.existing(
-					new RoundRectangle(this, 0, i * 80 - 100, 390, 50, 15, lightBackgroundColor)
-				);
-				const rankText = this.add
-					.text(-184, i * 80 - 100, `#${(i + 1).toString()}`, textStyle)
-					.setOrigin(0, 0.5)
-					.setAlign("left");
-				const playerText = this.add
-					.text(0, i * 80 - 100, player.DisplayName, textStyle)
-					.setAlign("Center")
-					.setOrigin(0.5, 0.5);
-				const statText = this.add
-					.text(184, i * 80 - 100, `${player.StatValue / 100}`, textStyle)
-					.setOrigin(1, 0.5)
-					.setAlign("right");
-				this.list.add([darkBg, lightBg, rankText, playerText, statText]);
-			});
-		});
+		PlayFabClient.GetLeaderboard(
+			{ StatisticName: "snowballs", StartPosition: 0, MaxResultsCount: 5 },
+			(error, result) => {
+				const darkBg = this.add.existing(new RoundRectangle(this, 0, 0, 480, 460, 15, darkBackgroundColor));
+				const players = result.data.Leaderboard;
+				players.forEach((player, i) => {
+					const lightBg = this.add.existing(
+						new RoundRectangle(this, 0, i * 80 - 160, 390, 50, 15, lightBackgroundColor)
+					);
+					const rankText = this.add
+						.text(-184, i * 80 - 160, `#${(i + 1).toString()}`, textStyle)
+						.setOrigin(0, 0.5)
+						.setAlign("left");
+					const playerText = this.add
+						.text(0, i * 80 - 160, player.DisplayName, textStyle)
+						.setAlign("Center")
+						.setOrigin(0.5, 0.5);
+					const statText = this.add
+						.text(184, i * 80 - 160, `${player.StatValue / 100}`, textStyle)
+						.setOrigin(1, 0.5)
+						.setAlign("right");
+					this.list.add([darkBg, lightBg, rankText, playerText, statText]);
+				});
+			}
+		);
 
 		const menuButtonUnderline = this.add.line(784, 584, 784, 584, 784, 584, 0xffffff).setAlpha(0);
 		const menuButton = this.add
