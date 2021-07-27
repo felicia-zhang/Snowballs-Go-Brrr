0ea2677772f287868f642ff70860a84960305886
Felicia Zhang <t-fezhang@microsoft.com>
2021-07-26 20:57:00 -0400

    move currency container out

--- src/phaser/game.ts
+++ src/phaser/game.ts
@@ -16,6 +16,7 @@ import ResetContainer from "./resetContainer";
 import StoreContainer from "./storeContainer";
 import CloseButton from "../utils/closeButton";
 import { showToast } from "./showToast";
+import CurrencyContainer from "./currencyContainer";
 
 class GameScene extends Phaser.Scene {
 	readonly syncDelay = 60000;
@@ -35,13 +36,12 @@ class GameScene extends Phaser.Scene {
 	resetBonusIcon: Phaser.GameObjects.Image;
 	storeContainer: StoreContainer;
 	resetContainer: ResetContainer;
+	currencyContainer: CurrencyContainer;
 	leaderboardContainer: LeaderboardContainer;
 	clickPenguinInstruction: Phaser.GameObjects.Text;
 	clickStoreInstruction: Phaser.GameObjects.Text;
 	itemsMap: { [key: string]: ItemDetail };
 	bundlesMap: { [key: number]: BundleDetail };
-	currencyContainer: Phaser.GameObjects.Container;
-	currencyItems: PlayFabClientModels.StoreItem[];
 	interactiveObjects: Phaser.GameObjects.GameObject[];
 	firstItemPrice: number;
 
@@ -61,14 +61,13 @@ class GameScene extends Phaser.Scene {
 		this.inventoryObjects = [];
 		this.inventoryTimers = [];
 		this.firstItemPrice = undefined;
-		this.currencyItems = [];
 		this.itemsMap = {};
 		this.bundlesMap = {};
 		this.interactiveObjects = [];
-		this.makeCurrencyContainer();
 		this.makePenguin();
 		this.storeContainer = this.add.existing(new StoreContainer(this, 400, 300));
 		this.resetContainer = this.add.existing(new ResetContainer(this, 400, 300));
+		this.currencyContainer = this.add.existing(new CurrencyContainer(this, 400, 300));
 		this.leaderboardContainer = this.add.existing(new LeaderboardContainer(this, 400, 300));
 
 		PlayFabClient.GetStoreItems({ StoreId: this.biomeId }, (error, result) => {
@@ -213,7 +212,7 @@ class GameScene extends Phaser.Scene {
 				.addHoverText("In-app Purchase")
 				.addCallback(() => {
 					this.interactiveObjects.forEach(object => object.disableInteractive());
-					this.showCurrencyContainer();
+					this.currencyContainer.show();
 				})
 		);
 		this.interactiveObjects.push(iapButton);
@@ -282,98 +281,6 @@ class GameScene extends Phaser.Scene {
 		if (!this.registry.has("IsSignedIn") || !this.registry.get("IsSignedIn")) {
 			this.scene.start("Signin");
 		}
-	}
-
-	makeCurrencyContainer() {
-		const overlay = this.add.rectangle(0, 0, 800, 600, 0x000000).setDepth(overlayDepth).setAlpha(0.6);
-		const mainBackground = this.add.existing(new RoundRectangle(this, 0, 0, 665, 255, 15, darkBackgroundColor));
-		const currencyList = this.add.container(0, 0, []);
-		const text = this.add
-			.text(
-				0,
-				160,
-				"*This is a mock of the payment process. \nNo real transaction will take place in the PlayFab backend.",
-				textStyle
-			)
-			.setAlign("center")
-			.setOrigin(0.5, 0.5);
-		this.currencyContainer = this.add
-			.container(400, 300, [overlay, mainBackground, currencyList, text])
-			.setAlpha(0)
-			.setDepth(popupDepth);
-		const closeButton = this.add.existing(
-			new CloseButton(this, 320, -115).addCallback(this.currencyContainer, () => {
-				const currencyList = this.currencyContainer.getAt(2) as Phaser.GameObjects.Container;
-				currencyList.removeAll(true);
-				this.currencyItems = [];
-			})
-		);
-		this.currencyContainer.add(closeButton);
-	}
-
-	showCurrencyContainer() {
-		PlayFabClient.GetStoreItems({ StoreId: "CurrenciesWithDiscount" }, (error, result) => {
-			result.data.Store.forEach((storeItem: PlayFabClientModels.StoreItem) => {
-				this.makeCurrency(storeItem);
-			});
-			const overlay = this.currencyContainer.getAt(0) as Phaser.GameObjects.Rectangle;
-			const bg = this.currencyContainer.getAt(1) as RoundRectangle;
-			const closeButton = this.currencyContainer.getAt(4) as CloseButton;
-			if (result.data.StoreId === "CurrenciesWithDiscount") {
-				overlay.setY(-25);
-				bg.height = 305;
-				bg.y = -25;
-				closeButton.setY(-165);
-				this.currencyContainer.setY(325);
-				const discountText = this.add
-					.text(
-						0,
-						-145,
-						"ONE TIME OFFER!!\nReceive 10% off ALL in-game items after your first icicle purchase!",
-						textStyle
-					)
-					.setAlign("center")
-					.setOrigin(0.5, 0.5);
-				const currencyList = this.currencyContainer.getAt(2) as Phaser.GameObjects.Container;
-				currencyList.add(discountText);
-			} else {
-				overlay.setY(0);
-				bg.height = 255;
-				bg.y = 0;
-				closeButton.setY(-115);
-				this.currencyContainer.setY(300);
-			}
-		});
-		this.add.tween({
-			targets: [this.currencyContainer],
-			ease: "Sine.easeIn",
-			duration: 500,
-			alpha: 1,
-			callbackScope: this,
-		});
-	}
-
-	makeCurrency(storeItem: PlayFabClientModels.StoreItem) {
-		const bundleDetail: BundleDetail = this.bundlesMap[storeItem.ItemId];
-		const usd = storeItem.VirtualCurrencyPrices.RM;
-
-		const index = this.currencyItems.length;
-		const x = 160 * index - 240;
-		this.currencyItems.push(storeItem);
-		const background = this.add.existing(new RoundRectangle(this, x, 0, 140, 220, 15, lightBackgroundColor));
-
-		const image = this.add.image(x, -10, storeItem.ItemId).setScale(0.7);
-		const nameText = this.add
-			.text(x, -90, bundleDetail.DisplayName.toUpperCase(), textStyle)
-			.setAlign("center")
-			.setOrigin(0.5, 0.5);
-		const button = this.add.existing(
-			new Button(this, x, 80)
-				.setText(`$ ${usd}.00`)
-				.addCallback(() => this.purchaseCurrency(bundleDetail, usd, button))
-		);
-		const currencyList = this.currencyContainer.getAt(2) as Phaser.GameObjects.Container;
-		currencyList.add([background, image, nameText, button]);
 	}
 
 	purchaseCurrency(bundleDetail: BundleDetail, usd: number, button: Button) {
