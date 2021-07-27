ed73523f2963d1cda84839801e9a5256f1fd6091
Felicia Zhang <t-fezhang@microsoft.com>
2021-07-27 15:32:32 -0400

    update store popup layout

--- src/phaser/containers/storeContainer.ts
+++ src/phaser/containers/storeContainer.ts
@@ -29,9 +29,9 @@ export default class StoreContainer extends Phaser.GameObjects.Container {
 		this.overlay = new Phaser.GameObjects.Rectangle(scene, 0, 0, 800, 600, 0x000000)
 			.setDepth(overlayDepth)
 			.setAlpha(0.6);
-		this.background = new RoundRectangle(scene, 0, 0, 420, 550, 15, darkDarkBlue);
+		this.background = new RoundRectangle(scene, 0, 0, 410, 570, 15, darkDarkBlue);
 		this.itemList = new Phaser.GameObjects.Container(scene, 0, 0, []);
-		this.closeButton = new Button(scene, 197, -262, false)
+		this.closeButton = new Button(scene, 192, -272, false)
 			.addIcon("close")
 			.addCloseCallback(this, this.scene.interactiveObjects, () => {
 				this.itemList.removeAll(true);
@@ -62,61 +62,61 @@ export default class StoreContainer extends Phaser.GameObjects.Container {
 		const maybeItemDiscountPrice = storeItem.VirtualCurrencyPrices.SB;
 
 		const index = this.storeItems.length;
-		const y = -210 + index * 105;
+		const y = -220 + index * 110;
 		this.storeItems.push(storeItem);
-		const image = new Phaser.GameObjects.Image(this.scene, -145, y, storeItem.ItemId).setScale(0.35);
+		const image = new Phaser.GameObjects.Image(this.scene, -142, y, storeItem.ItemId).setScale(0.35);
 		const nameText = new Phaser.GameObjects.Text(
 			this.scene,
-			-100,
-			y - 27,
+			-95,
+			y - 20,
 			itemDetail.DisplayName.toUpperCase(),
 			normalTextStyle
 		)
 			.setAlign("left")
 			.setOrigin(0, 0.5);
 		const description = itemDetail.Description.split("\n");
+		const effectText = new Phaser.GameObjects.Text(this.scene, -95, y, description[2], smallTextStyle)
+			.setAlign("left")
+			.setOrigin(0, 0.5);
 		const descriptionText = new Phaser.GameObjects.Text(
 			this.scene,
-			-100,
-			y + 5,
-			wrapString(description[0], 28),
+			-95,
+			y + 30,
+			wrapString(description[0], 50),
 			smallTextStyle
 		)
 			.setAlign("left")
 			.setOrigin(0, 0.5);
-		const effectText = new Phaser.GameObjects.Text(this.scene, -100, y + 30, description[2], smallTextStyle)
-			.setAlign("left")
-			.setOrigin(0, 0.5);
-		const background = new RoundRectangle(this.scene, 0, y, 380, 90, 15, darkBlue);
+		const background = new RoundRectangle(this.scene, 0, y, 380, 95, 15, darkBlue);
 
-		const button = new Button(this.scene, 170, y)
+		const button = new Button(this.scene, 170, y - 12)
 			.addIcon("snowball")
 			.setText(`${numberWithCommas(maybeItemDiscountPrice / 100)} x`)
 			.addCallback(() => this.scene.purchaseItem(itemDetail, maybeItemDiscountPrice, storeId, button));
-		button.setX(180 - button.background.width / 2);
+		button.setX(170 - button.background.width / 2);
 		this.itemList.add([background, image, nameText, descriptionText, effectText, button]);
 
 		if (storeId === `${this.scene.biomeId}WithDiscount`) {
-			button.setY(y + 10);
+			button.setY(y - 2);
 
 			const fullPriceText = new Phaser.GameObjects.Text(
 				this.scene,
-				141,
-				y - 25,
+				131,
+				y - 32,
 				`${numberWithCommas(storeItem.CustomData.FullPrice / 100)} x`,
 				smallTextStyle
 			)
 				.setAlign("right")
 				.setOrigin(1, 0.5);
-			const oldSnowballIcon = new Phaser.GameObjects.Image(this.scene, 155, y - 25, "snowball").setScale(0.09);
+			const oldSnowballIcon = new Phaser.GameObjects.Image(this.scene, 145, y - 32, "snowball").setScale(0.09);
 			const line = new Phaser.GameObjects.Line(
 				this.scene,
 				0,
 				0,
-				170,
-				y - 25,
+				165,
+				y - 32,
 				205 + fullPriceText.width,
-				y - 25,
+				y - 32,
 				0xffffff
 			).setOrigin(1, 0.5);
 			this.itemList.add([fullPriceText, oldSnowballIcon, line]);
