25cfa4972cd177f5dc8ec33c7e0a178b28b0371b
Felicia Zhang <t-fezhang@microsoft.com>
2021-07-28 09:53:45 -0400

    update reset popup layout

--- src/phaser/containers/resetContainer.ts
+++ src/phaser/containers/resetContainer.ts
@@ -1,6 +1,15 @@
 import RoundRectangle from "phaser3-rex-plugins/plugins/roundrectangle.js";
 import Button from "../../utils/button";
-import { darkDarkBlue, darkBlue, overlayDepth, popupDepth, normalTextStyle } from "../../utils/constants";
+import {
+	darkDarkBlue,
+	darkBlue,
+	overlayDepth,
+	popupDepth,
+	normalTextStyle,
+	smallTextStyle,
+	largeFontSize,
+	fontFamily,
+} from "../../utils/constants";
 import { numberWithCommas } from "../../utils/stringFormat";
 import GameScene from "../scenes/game";
 
@@ -8,10 +17,14 @@ export default class ResetContainer extends Phaser.GameObjects.Container {
 	overlay: Phaser.GameObjects.Rectangle;
 	background: RoundRectangle;
 	lightBackground: RoundRectangle;
-	description: Phaser.GameObjects.Text;
+	title: Phaser.GameObjects.Text;
 	closeButton: Button;
+	snowballContainer: Phaser.GameObjects.Container;
+	snowballLable: Phaser.GameObjects.Text;
 	snowballText: Phaser.GameObjects.Text;
 	snowballIcon: Phaser.GameObjects.Image;
+	resetBonusContainer: Phaser.GameObjects.Container;
+	resetBonusLable: Phaser.GameObjects.Text;
 	resetBonusText: Phaser.GameObjects.Text;
 	resetBonusIcon: Phaser.GameObjects.Image;
 	footnote: Phaser.GameObjects.Text;
@@ -25,36 +38,61 @@ export default class ResetContainer extends Phaser.GameObjects.Container {
 		this.overlay = new Phaser.GameObjects.Rectangle(scene, 0, 0, 800, 600, 0x000000)
 			.setDepth(overlayDepth)
 			.setAlpha(0.6);
-		this.background = new RoundRectangle(scene, 0, 0, 380, 340, 15, darkDarkBlue);
-		this.lightBackground = new RoundRectangle(scene, 0, 45, 340, 210, 15, darkBlue);
-		this.description = new Phaser.GameObjects.Text(
+		this.background = new RoundRectangle(scene, 0, 0, 510, 235, 15, darkDarkBlue);
+		this.lightBackground = new RoundRectangle(scene, 0, 15, 460, 125, 15, darkBlue);
+		this.title = new Phaser.GameObjects.Text(
 			scene,
 			0,
-			-150,
-			`Reset game to earn 0.01 reset bonus\nfor every 10,000 snowballs in your balance.\nThe reset bonus will be applied to\nmanual clicks and item effects.\n\nYour current snowball balance is:\n\n\nReset will award you with:`,
+			-100,
+			"Earn 0.01 reset bonus per 10,000 snowballs\nReset bonus will be applied to clicks and item effects.",
 			normalTextStyle
 		)
 			.setAlign("center")
 			.setOrigin(0.5, 0);
-		this.snowballText = new Phaser.GameObjects.Text(scene, 0, -15, "", normalTextStyle)
+
+		this.snowballLable = new Phaser.GameObjects.Text(scene, 0, 0, "Snowball Balance", smallTextStyle)
 			.setAlign("left")
 			.setOrigin(0, 0.5);
-		this.snowballIcon = new Phaser.GameObjects.Image(scene, 0, -15, "snowball").setScale(0.15).setOrigin(1, 0.5);
-		this.resetBonusText = new Phaser.GameObjects.Text(scene, 0, 45, "", normalTextStyle)
+		this.snowballText = new Phaser.GameObjects.Text(scene, 0, 30, "", {
+			fontFamily: fontFamily,
+			fontSize: largeFontSize,
+		})
 			.setAlign("left")
 			.setOrigin(0, 0.5);
-		this.resetBonusIcon = new Phaser.GameObjects.Image(scene, 0, 45, "star").setScale(0.15).setOrigin(1, 0.5);
+		this.snowballIcon = new Phaser.GameObjects.Image(scene, 0, 30, "snowball").setScale(0.15).setOrigin(0, 0.5);
+		this.snowballContainer = new Phaser.GameObjects.Container(scene, -200, -30, [
+			this.snowballLable,
+			this.snowballText,
+			this.snowballIcon,
+		]);
+
+		this.resetBonusLable = new Phaser.GameObjects.Text(scene, 0, 0, "Reset Bonus", smallTextStyle)
+			.setAlign("left")
+			.setOrigin(0, 0.5);
+		this.resetBonusText = new Phaser.GameObjects.Text(scene, 0, 30, "", {
+			fontFamily: fontFamily,
+			fontSize: largeFontSize,
+		})
+			.setAlign("left")
+			.setOrigin(0, 0.5);
+		this.resetBonusIcon = new Phaser.GameObjects.Image(scene, 0, 30, "star").setScale(0.15).setOrigin(0, 0.5);
+		this.resetBonusContainer = new Phaser.GameObjects.Container(scene, 80, -30, [
+			this.resetBonusLable,
+			this.resetBonusText,
+			this.resetBonusIcon,
+		]);
+
 		this.footnote = new Phaser.GameObjects.Text(
 			scene,
 			0,
-			180,
-			"*Reset will NOT affect\nyour in-app purchase history\nor your icicle balance",
-			normalTextStyle
+			87,
+			"*Reset will NOT affect your in-app purchase history or your icicle balance",
+			smallTextStyle
 		)
 			.setAlign("center")
 			.setOrigin(0.5, 0);
-		this.resetButton = new Button(scene, 0, 114).setText("RESET").addCallback(() => scene.reset(this.resetButton));
-		this.closeButton = new Button(scene, 177, -157, false)
+		this.resetButton = new Button(scene, 0, 45).setText("RESET").addCallback(() => scene.reset(this.resetButton));
+		this.closeButton = new Button(scene, 242, -104.5, false)
 			.addIcon("close")
 			.addCloseCallback(this, this.scene.interactiveObjects, () => {
 				this.resetButton.resetButton();
@@ -64,11 +102,9 @@ export default class ResetContainer extends Phaser.GameObjects.Container {
 			this.overlay,
 			this.background,
 			this.lightBackground,
-			this.description,
-			this.snowballText,
-			this.snowballIcon,
-			this.resetBonusText,
-			this.resetBonusIcon,
+			this.title,
+			this.snowballContainer,
+			this.resetBonusContainer,
 			this.footnote,
 			this.resetButton,
 			this.closeButton,
@@ -82,14 +118,10 @@ export default class ResetContainer extends Phaser.GameObjects.Container {
 		const resetBonus = Math.floor(snowballBalance / 1000000);
 
 		this.snowballText.setText(`${numberWithCommas(snowballBalance / 100)} x`);
-		const snowballX = (this.snowballText.width + this.snowballIcon.displayWidth + 6) / 2;
-		this.snowballText.setX(-snowballX);
-		this.snowballIcon.setX(snowballX);
+		this.snowballIcon.setX(this.snowballText.width + 6);
 
 		this.resetBonusText.setText(`${numberWithCommas(resetBonus / 100)} x`);
-		const resetX = (this.resetBonusText.width + 36) / 2;
-		this.resetBonusText.setX(-resetX);
-		this.resetBonusIcon.setX(resetX + 7.5);
+		this.resetBonusIcon.setX(this.resetBonusText.width - 1.5);
 
 		this.scene.add.tween({
 			targets: [this],
